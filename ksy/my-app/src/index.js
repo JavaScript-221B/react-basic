79087e589004e5cb8384195a77d5442317f2b7b1
SooYeonKim <lovesu5632@gmail.com>
2022-06-01 13:22:49 +0900

    Add: 댓글 컴포넌트 만들기

--- ksy/my-app/src/index.js
+++ ksy/my-app/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -14,14 +15,21 @@ import Clock from './chapter_04/Clock';
 //   </React.StrictMode>
 // );
 
-setInterval(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}, 1000);
+// setInterval(() => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }, 1000);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
