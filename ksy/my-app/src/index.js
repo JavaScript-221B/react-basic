31dddebb5900d8c5a0dd04a024a075c6e5b86a17
SooYeonKim <lovesu5632@gmail.com>
2022-06-15 17:54:52 +0900

    Add: Context를 사용하여 테마 변경 기능 만들기

--- ksy/my-app/src/index.js
+++ ksy/my-app/src/index.js
@@ -15,6 +15,7 @@ import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from './chapter_11/SighUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -88,9 +89,16 @@ import ProfileCard from './chapter_13/ProfileCard';
 //   document.getElementById('root')
 // );
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ProfileCard />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>,
   document.getElementById('root')
 );
