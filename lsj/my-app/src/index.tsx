dba44b0c4e249ed6328212e4baf8961d9d3a1660
seongjaee <labyrinthird@gmail.com>
2022-06-15 11:42:14 +0900

    Add: 사용자 정보 입력 받기

--- lsj/my-app/src/index.tsx
+++ lsj/my-app/src/index.tsx
@@ -14,12 +14,13 @@ import ConfirmButton from "./chapter_08/ConfirmButton";
 import Toolbar from "./chapter_09/Toolbar";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
+import SignUp from "./chapter_11/SignUp";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<AttendanceBook />);
+root.render(<SignUp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
