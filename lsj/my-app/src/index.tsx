7a2927f23c4e23dfe7045813bd40a0010d30f762
seongjaee <labyrinthird@gmail.com>
2022-06-16 23:26:27 +0900

    Add: Context를 사용하여 테마 변경 기능 만들기

--- lsj/my-app/src/index.tsx
+++ lsj/my-app/src/index.tsx
@@ -17,12 +17,13 @@ import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
 import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<ProfileCard />);
+root.render(<DarkOrLight />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
